ddd2f8992e22ed006eb63daece32678f3207a39b
Josuepf <josuepontes000@gmail.com>
2025-07-03 21:41:41 -0300

    Corrigido CORS no backend

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,9 @@ const PORT = process.env.PORT || 3000;
 
 const DATA_FILE = path.join(__dirname, 'data.json');
 
+const cors = require('cors');
+app.use(cors());
+
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
